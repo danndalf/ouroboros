c7af6d567ea1cbe931f955cab0766279e7945b25
Dann Stayskal <dann@stayskal.com>
2013-05-16 13:51:54 -0700

    Moving configuration blocks to before create_symlibk in deploy procedure

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,16 +12,19 @@ set :use_sudo, false
 
 # if you want to clean up old releases on each deploy uncomment this:
 #after "deploy:restart", "deploy:cleanup"
+before "deploy:create_symlink", "deploy:update_config_files"
 
 # If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
    task :start do ; end
    task :stop do ; end
-   task :restart, :roles => :app, :except => { :no_release => true } do
-   	 run "cd #{File.join(current_path)}; bundle install && bundle install --deployment"
-   	 run "cd #{File.join(current_path)}; cp /home/dann/s3.yml config/"
-   	 run "cd #{File.join(current_path)}; cp /home/dann/database.yml config/"
-   	 run "cd #{File.join(current_path)}; RAILS_ENV=production rake db:migrate"
+	 task :update_config_files, :roles => :app do
+   	 run "cd #{File.join(release_path)}; bundle install && bundle install --deployment"
+   	 run "cd #{File.join(release_path)}; cp /home/dann/s3.yml config/"
+   	 run "cd #{File.join(release_path)}; cp /home/dann/database.yml config/"
+   	 run "cd #{File.join(release_path)}; RAILS_ENV=production rake db:migrate"
+	 end
+	 task :restart, :roles => :app, :except => { :no_release => true } do
      run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
    end
 end
