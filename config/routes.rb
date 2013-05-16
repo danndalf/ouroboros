c710e7924bc7652fac0e0051bff3f98b39754f30
Jack C <chattj@gmail.com>
2013-05-16 13:36:03 -0700

    add donations page

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Ouroboros::Application.routes.draw do
 
   resources :stories
 
+  get '/donations' => 'donations#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
