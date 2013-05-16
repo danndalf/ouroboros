48b0f0dcde6f3950f623b56c1cafb182fc483ef9
Dann Stayskal <dann@stayskal.com>
2013-05-15 17:13:07 -0700

    Finishing out welcome page; CSS touch-ups

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,8 @@ class WelcomeController < ApplicationController
 
 	def index
 		@stories = Story.limit(10).find(:all)
+		@photos = Photo.limit(2).order('created_at DESC').find(:all)
+		@recordings = Recording.limit(2).find(:all)
 	end
 	
 end
